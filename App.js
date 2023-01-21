7b963532775a9817448334c4f91c559f0e71b812
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-01-21 11:30:49 +0100

    showed the goal list

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// jshint esversion:6
+
+import {useState} from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals(currentCourseGoals => [
+      ...currentCourseGoals, enteredGoalText,
+    ]);
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+
+      <View style={styles.inputContainer}>
+        {/* <Text>My 2023 Goal</Text> */}
+        <TextInput style={styles.textInput} placeholder='Enter a Goal' 
+        onChangeText={goalInputHandler} 
+        />
+        <Button title='Add goal' onPress={addGoalHandler}
+        />
+      </View>
+
+      <View style={styles.goalsContainer}>
+        { courseGoals.map((goal) => <Text key={goal}>{goal}</Text> )}
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+   marginBottom: 24,
+    borderBottomWidth: 1, 
+    borderBottomColor: '#ccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '75%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalsContainer: {
+    flex: 5,
+  }
 });
